668c83af993d31ce3c269944b2ccc54d027d3cf8
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-26 00:14:38 +0300

    implement cart actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,7 +17,7 @@ const router = async () => {
   (request.id ? '/:id' : '') + 
   (request.verb ? `/${request.verb}` : '')
   const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen
-
+console.log('screen',screen)
   const main = document.getElementById('main-container');
   main.innerHTML = await screen.render();
   await screen.after_render();
