c322d79d638c948ef1b4074b52459c768a1a6a42
Burak Özkan <burak.ozkan@path.com.tr>
2021-11-15 23:52:52 +0300

    add sign in with jwt

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import User from '../models/userModel';
+import expressAsyncHandler from 'express-async-handler';
+import { generateToken } from '../util';
 
 const userRouter = express.Router();
 
-userRouter.get('/createadmin', async (req, res) => {
+userRouter.get('/createadmin', expressAsyncHandler(async (req, res) => {
   try {
     const user = new User({
       name: 'admin',
@@ -16,6 +18,21 @@ userRouter.get('/createadmin', async (req, res) => {
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
-});
+}));
+
+userRouter.post("/signin", expressAsyncHandler(async (req, res) => {
+  const signinUser = await User.findOne({ email: req.body.email, password: req.body.password });
+  if (!signinUser) {
+    return res.status(401).send({ message: "Invalid email or password" });
+  } else {
+    res.send({
+      _id: signinUser._id,
+      name: signinUser.name,
+      email: signinUser.email,
+      isAdmin: signinUser.isAdmin,
+      token: generateToken(signinUser),
+    })
+  }
+}));
 
 export default userRouter;
